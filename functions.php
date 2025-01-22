dba550896b354fbb8895db7af24c11efca7a9548
Maiaozinho <gustavomaia334@gmail.com>
2025-01-22 14:19:09 -0300

    feat: first commit

---
+++ functions.php
@@ -0,0 +1,5 @@
+<?php
+
+function wpdevs_load_scripts() {
+    wp_enquere_style();
+}
---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width= , initial-scale=1.0">
+</head>
+<body <?php body_class(); ?>>
+    <p>Testando os paragráfos</p>
+    <div id="page" class="site">
+    </div>
+    <header>
+        <section class="top-bar">
+            <div class="logo">
+                Logo
+            </div>
+            <div class="searchbox">
+                Search
+            </div>
+        </section>
+        <section class="menu-area">
+            <nav class="main-menu">
+                Menu
+            </nav>
+        </section>
+    </header>
